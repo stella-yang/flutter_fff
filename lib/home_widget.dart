d326ab87b3056fd3b8c619572be268710be2f654
Stella <stella.lan.yang@gmail.com>
2019-09-29 17:03:48 -0400

    Add list of friends

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/onlinefriends_widget.dart';
 import 'filler_widget.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -9,11 +10,15 @@ class Home extends StatefulWidget {
   }
 }
 
+Color background = new Color(0xffFFF5E1);
+Color navBar = new Color(0xffFFF1D4);
+Color lightgray = new Color(0xff8a8a8a);
+
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
   final List<Widget> _children = [
     FillerWidget(Colors.white),
-    FillerWidget(Colors.green),
+    OnlineFriendsWidget(background),
     FillerWidget(Colors.blue),
   ];
   final List<String> _titles = [
@@ -21,18 +26,20 @@ class _HomeState extends State<Home> {
     "Online Friends",
     "Outgoing Requests",
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffFFF5E1),
+      backgroundColor: background,
       appBar: AppBar(
         title: Text(
           _titles[_currentIndex],
           style: TextStyle(
             color: Colors.black,
+            fontSize: 28,
           ),
-          ),
-        backgroundColor: Color(0xffFFF5E1),
+        ),
+        backgroundColor: background,
         elevation: 0.0,
         iconTheme: new IconThemeData(color: Colors.black),
       ),
@@ -47,7 +54,7 @@ class _HomeState extends State<Home> {
             DrawerHeader(
               child: Text('Drawer Header'),
               decoration: BoxDecoration(
-                color: Colors.blue,
+                color: Colors.orange,
               ),
             ),
             ListTile(
@@ -70,23 +77,52 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTabTapped,
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.mail),
-            title: new Text('Incoming Requests'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.person),
-            title: new Text('Online Friends'),
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.paperPlane),
-              title: Text('Outgoing Requests'))
-        ],
-      ),
+      bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+              // sets the background color of the `BottomNavigationBar`
+              canvasColor: navBar,
+              // sets the active color of the `BottomNavigationBar` if `Brightness` is light
+              primaryColor: Colors.orange,
+              textTheme: Theme.of(context)
+                  .textTheme
+                  .copyWith(caption: new TextStyle(color: lightgray))),
+          // sets the inactive color of the `BottomNavigationBar`
+          child: SizedBox(
+            height: 64,
+            child: BottomNavigationBar(
+              onTap: onTabTapped,
+              currentIndex: _currentIndex,
+              items: [
+                BottomNavigationBarItem(
+                    icon: new Icon(Icons.mail),
+                    title: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 6),
+                      child: Text(
+                        'Incoming Requests',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    )),
+                BottomNavigationBarItem(
+                    icon: new Icon(Icons.person),
+                    title: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 6),
+                      child: Text(
+                        'Incoming Requests',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    )),
+                BottomNavigationBarItem(
+                    icon: Icon(FontAwesomeIcons.paperPlane),
+                    title: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 6),
+                      child: Text(
+                        'Incoming Requests',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ))
+              ],
+            ),
+          )),
     );
   }
 
